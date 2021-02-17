d4de226a9176eebe567877a52638f1bb58336c32
gabbas5 <gabbas5@hotmail.com>
2021-02-17 23:19:40 +0500

    data successfully posted

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
-
+//projectData = {name:'abbas',city:'Islamabad',age:60,family:['naish','Tashfa','naimah'],car:{type1:'toyota',type2:'acquva'}};
+let appJSON = '{"coord":{"lon":73.1338,"lat":33.7104},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01n"}],"base":"stations","main":{"temp":288.71,"feels_like":286.85,"temp_min":288.71,"temp_max":288.71,"pressure":1014,"humidity":66},"visibility":10000,"wind":{"speed":2.43,"deg":35},"clouds":{"all":0},"dt":1613412613,"sys":{"type":3,"id":2007435,"country":"PK","sunrise":1613353927,"sunset":1613393481},"timezone":18000,"id":1162015,"name":"Islamabad","cod":200}';
+ let appData=JSON.parse(appJSON);  
+ 
 // Require Express to run server and routes
-
+  const express=require('express');
 // Start up an instance of app
-
-/* Middleware*/
+    const app = express();
+    /* Middleware*/
+    const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-// Cors for cross origin allowance
-
+// Cors for cross origin allowance and its use
+const cors= require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
-// Setup Server
+  // set port for server
+  const port=8000;
+  // Setup Server
+  const server=app.listen(port,listening);
+  // call back to debuging
+   function listening(){
+      console.log('server running');
+      console.log(`running on localhost:${port}`);
+  }
+   //get route
+  // app.get('/',function(req,res){
+   //   res.send('Hello World');
+  // });
+   // another get route
+   app.get('/all',function(req,res){
+     res.send(appData);
+    // console.log(appData.sys.country);
+    // console.log(appData.name);
+   });
+   const data=[];
+   app.post('/add',addMovie);
+   function addMovie(req,res){
+      // console.log(req.body);
+       data.push(req.body);
+       console.log(data);
+      
+   }
--- website/app.js
+++ website/app.js
@@ -1,5 +1,39 @@
 /* Global Variables */
-
+//let baseURL= 'http://api.openweathermap.org/data/2.5/weather?q=';
+//let apiKey='&appid=def5df937bc9d3ecbb89d2dfdfa49ed2';
+/* to get value of API from the open weather */
+//document.getElementById('generate').addEventListener('click',performAction);
+//function performAction(e){
+//  const newCity=document.getElementById('zip').value;
+//     getCityWeather(baseURL,newCity,apiKey)
+ //   .then(function(data){
+ //       console.log(data);
+ //     postData('/add',{data}); 
+ //    })
+          
+//}
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+console.log(newDate);
+// data for post request
+const postData= async (url='',data={})=>{
+console.log(data);
+const response=await fetch(url, {
+    method:'POST',
+    credentials:"same-origin",
+    headers:{
+        'Content-Type': 'application/json',
+    },
+    //body data type must match 'content-Type' header
+    body:JSON.stringify(data),
+});
+try{
+    const newData = await response.json();
+    console.log(newData);
+    return newData;
+}catch(error){
+    console.log("error",error);
+}
+}
+postData('/add',{answer:42});
